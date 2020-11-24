a5c96c07cec4a0e5af735a7ca2758736b76e2cef
Aryton Hoi <aryton.hoi@bitsighttech.com>
2020-11-24 01:22:44 -0500

    reverting to token auth

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -8,57 +8,6 @@ firebase.initializeApp(firebaseConfig);
 const { validateLoginData } = require("../util/validators");
 
 // log user in
-// exports.login = (req, res) => {
-//   try {
-//     req = fixFormat(req);
-//   } catch (e) {
-//     return res.status(400).json({ error: "Invalid JSON." });
-//   }
-//   var user = {
-//     email: "",
-//     password: "",
-//   };
-//   // turn username into email
-//   try {
-//     user = {
-//       email: req.body.username.toString().concat("@email.com"),
-//       password: req.body.password.toString(),
-//     };
-//   } catch (e) {
-//     return res.status(400).json({
-//       error: "Incomplete JSON, username and password fields required.",
-//     });
-//   }
-//   // validate data
-//   const { valid, errors } = validateLoginData(user);
-//   if (!valid) return res.status(400).json(errors);
-
-//   firebase
-//     .auth()
-//     .signInWithEmailAndPassword(user.email, user.password)
-//     .then((data) => {
-//       return data.user.getIdToken();
-//     })
-//     .then((token) => {
-//       return res.json({ token });
-//     })
-//     .catch((err) => {
-//       console.error(err);
-//       if (err.code === "auth/wrong-password") {
-//         return res
-//           .status(403)
-//           .json({ general: "Wrong password, please try again" });
-//       } else if (err.code === "auth/user-not-found") {
-//         return res
-//           .status(403)
-//           .json({ general: "Wrong username, please try again" });
-//       } else {
-//         return res.status(500).json({ error: err.code });
-//       }
-//     });
-// };
-
-// log user in (cookie)
 exports.login = (req, res) => {
   try {
     req = fixFormat(req);
@@ -84,46 +33,97 @@ exports.login = (req, res) => {
   const { valid, errors } = validateLoginData(user);
   if (!valid) return res.status(400).json(errors);
 
-  // When the user signs in with email and password.
   firebase
     .auth()
     .signInWithEmailAndPassword(user.email, user.password)
-    .then((userData) => {
-      // Get the user's ID token as it is needed to exchange for a session cookie.
-      return userData.user.getIdToken().then((idToken) => {
-        // expires in 14 days
-        const expiresIn = 1000 * 60 * 60 * 24 * 14;
-        admin
-          .auth()
-          .createSessionCookie(idToken, { expiresIn })
-          .then((sessionCookie) => {
-            // Set cookie policy for session cookie.
-            const options = {
-              maxAge: expiresIn,
-              httpOnly: true,
-              secure: false, // set to true for HTTPS, false for HTTP
-              // Domain: "us-central1-fir-db-d2d47.cloudfunctions.net",
-            };
-            res.cookie("session", sessionCookie, options);
-            return res.status(200).json({ status: "Login successful" });
-          });
-      });
+    .then((data) => {
+      return data.user.getIdToken();
+    })
+    .then((token) => {
+      return res.json({ token });
     })
     .catch((err) => {
       console.error(err);
       if (err.code === "auth/wrong-password") {
         return res
           .status(403)
-          .json({ general: "Password incorrect, please try again" });
+          .json({ general: "Wrong password, please try again" });
       } else if (err.code === "auth/user-not-found") {
         return res
           .status(403)
-          .json({ general: "Username incorrect, please try again" });
+          .json({ general: "Wrong username, please try again" });
       } else {
         return res.status(500).json({ error: err.code });
       }
     });
 };
+
+// log user in (cookie)
+// exports.login = (req, res) => {
+//   try {
+//     req = fixFormat(req);
+//   } catch (e) {
+//     return res.status(400).json({ error: "Invalid JSON." });
+//   }
+//   var user = {
+//     email: "",
+//     password: "",
+//   };
+//   // turn username into email
+//   try {
+//     user = {
+//       email: req.body.username.toString().concat("@email.com"),
+//       password: req.body.password.toString(),
+//     };
+//   } catch (e) {
+//     return res.status(400).json({
+//       error: "Incomplete JSON, username and password fields required.",
+//     });
+//   }
+//   // validate data
+//   const { valid, errors } = validateLoginData(user);
+//   if (!valid) return res.status(400).json(errors);
+
+//   // When the user signs in with email and password.
+//   firebase
+//     .auth()
+//     .signInWithEmailAndPassword(user.email, user.password)
+//     .then((userData) => {
+//       // Get the user's ID token as it is needed to exchange for a session cookie.
+//       return userData.user.getIdToken().then((idToken) => {
+//         // expires in 14 days
+//         const expiresIn = 1000 * 60 * 60 * 24 * 14;
+//         admin
+//           .auth()
+//           .createSessionCookie(idToken, { expiresIn })
+//           .then((sessionCookie) => {
+//             // Set cookie policy for session cookie.
+//             const options = {
+//               maxAge: expiresIn,
+//               httpOnly: true,
+//               secure: false, // set to true for HTTPS, false for HTTP
+//               // Domain: "us-central1-fir-db-d2d47.cloudfunctions.net",
+//             };
+//             res.cookie("session", sessionCookie, options);
+//             return res.status(200).json({ status: "Login successful" });
+//           });
+//       });
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//       if (err.code === "auth/wrong-password") {
+//         return res
+//           .status(403)
+//           .json({ general: "Password incorrect, please try again" });
+//       } else if (err.code === "auth/user-not-found") {
+//         return res
+//           .status(403)
+//           .json({ general: "Username incorrect, please try again" });
+//       } else {
+//         return res.status(500).json({ error: err.code });
+//       }
+//     });
+// };
 
 // revoke cookie
 exports.logout = (req, res) => {
--- functions/util/fbAuth.js
+++ functions/util/fbAuth.js
@@ -1,58 +1,19 @@
 const { admin, db } = require("./admin");
 
-module.exports = FBAuth = (req, res, next) => {
-  let sessionCookie;
-  try {
-    sessionCookie = req.cookies.session;
-  } catch (err) {
-    console.error("Authentication cookie not provided");
-    return res.status(403).json({ error: "Unauthorized" });
-  }
-
-  admin
-    .auth()
-    .verifySessionCookie(sessionCookie, true)
-    .then((decodedClaims) => {
-      req.user = decodedClaims;
-      return db
-        .collection("users")
-        .where("userId", "==", req.user.uid)
-        .limit(1)
-        .get();
-    })
-    .then((data) => {
-      if (data.docs.length < 1) {
-        console.error("User doesn't exist in database");
-        return res.status(403).json({ error: "User doesn't exist" });
-      }
-      req.user.userId = data.docs[0].id;
-      req.user.email = data.docs[0].data().email;
-      req.user.isAdmin = data.docs[0].data().isAdmin;
-      return next();
-    })
-    .catch((err) => {
-      console.error("Error while verifying cookie ", err);
-      return res.status(403).json(err);
-    });
-};
-
 // module.exports = FBAuth = (req, res, next) => {
-//   let idToken;
-//   if (
-//     req.headers.authorization &&
-//     req.headers.authorization.startsWith("Bearer ")
-//   ) {
-//     idToken = req.headers.authorization.split("Bearer ")[1];
-//   } else {
-//     console.error("No token found");
+//   let sessionCookie;
+//   try {
+//     sessionCookie = req.cookies.session;
+//   } catch (err) {
+//     console.error("Authentication cookie not provided");
 //     return res.status(403).json({ error: "Unauthorized" });
 //   }
 
 //   admin
 //     .auth()
-//     .verifyIdToken(idToken)
-//     .then((decodedToken) => {
-//       req.user = decodedToken;
+//     .verifySessionCookie(sessionCookie, true)
+//     .then((decodedClaims) => {
+//       req.user = decodedClaims;
 //       return db
 //         .collection("users")
 //         .where("userId", "==", req.user.uid)
@@ -70,7 +31,46 @@ module.exports = FBAuth = (req, res, next) => {
 //       return next();
 //     })
 //     .catch((err) => {
-//       console.error("Error while verifying token ", err);
+//       console.error("Error while verifying cookie ", err);
 //       return res.status(403).json(err);
 //     });
 // };
+
+module.exports = FBAuth = (req, res, next) => {
+  let idToken;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer ")
+  ) {
+    idToken = req.headers.authorization.split("Bearer ")[1];
+  } else {
+    console.error("No token found");
+    return res.status(403).json({ error: "Unauthorized" });
+  }
+
+  admin
+    .auth()
+    .verifyIdToken(idToken)
+    .then((decodedToken) => {
+      req.user = decodedToken;
+      return db
+        .collection("users")
+        .where("userId", "==", req.user.uid)
+        .limit(1)
+        .get();
+    })
+    .then((data) => {
+      if (data.docs.length < 1) {
+        console.error("User doesn't exist in database");
+        return res.status(403).json({ error: "User doesn't exist" });
+      }
+      req.user.userId = data.docs[0].id;
+      req.user.email = data.docs[0].data().email;
+      req.user.isAdmin = data.docs[0].data().isAdmin;
+      return next();
+    })
+    .catch((err) => {
+      console.error("Error while verifying token ", err);
+      return res.status(403).json(err);
+    });
+};
